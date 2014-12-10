ef4585fc5b0ff36d699fe02ba8052db71630c123
Inash Zubair <inash@leptone.com>
2014-12-11 02:12:22 +0500

    initial commit

---
+++ src/org/osgi/tutorial/BundleCounterActivator.java
@@ -0,0 +1,42 @@
+package org.osgi.tutorial;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+
+public class BundleCounterActivator implements BundleActivator, BundleListener {
+
+	private BundleContext context;
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		this.context = context;
+		context.addBundleListener(this);
+		printBundleCount();
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		context.removeBundleListener(this);
+	}
+	
+	@Override
+	public void bundleChanged(BundleEvent event) {
+		switch (event.getType()) {
+		case BundleEvent.INSTALLED:
+			System.out.println("Bundle installed");
+			printBundleCount();
+			break;
+		case BundleEvent.UNINSTALLED:
+			System.out.println("Bundle uninstalled");
+			printBundleCount();
+			break;
+		}
+	}
+	
+	private void printBundleCount() {
+		int count = context.getBundles().length;
+		System.out.println("There are currently " + count + " bundles");
+	}
+}
---
+++ src/org/osgi/tutorial/HelloWorldActivator.java
@@ -0,0 +1,17 @@
+package org.osgi.tutorial;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class HelloWorldActivator implements BundleActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Hello, World!");
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Goodbye, World!");
+	}
+}
